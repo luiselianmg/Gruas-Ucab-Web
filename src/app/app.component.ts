8626aba3e37b6d64b305474c7ee7346c247a5136
ACKRONNE <ACKRONNE@GMAIL.COM>
2024-12-25 14:49:41 -0400

    ✨ FEAT:F-GRUAS-DirectionsByUser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,10 @@ import { GoogleMapsModule } from "@angular/google-maps";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GoogleMapsModule],
+  imports: [
+    RouterOutlet, 
+    GoogleMapsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
