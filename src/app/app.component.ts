5c56e35f3c95ff73b8ca5cacc84cea312cfc4c54
ACKRONNE <ACKRONNE@GMAIL.COM>
2024-12-25 14:33:54 -0400

    ✨ FEAT:F-GRUAS-GoogleMapsIntegration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-
-
+import { GoogleMapsModule } from "@angular/google-maps";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,],
+  imports: [RouterOutlet, GoogleMapsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'UCAB-GRUAS';
-
 }
