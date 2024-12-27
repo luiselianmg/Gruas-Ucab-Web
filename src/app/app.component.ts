1b57e0dd83e9be3b3810d09b37b5feda2c82477d
ACKRONNE <ACKRONNE@GMAIL.COM>
2024-12-27 16:04:35 -0400

    ✨ FEAT:F-GRUAS-AddMapMarkers&RouteCalculation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GoogleMapsModule } from "@angular/google-maps";
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet, 
-    GoogleMapsModule
+    GoogleMapsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
