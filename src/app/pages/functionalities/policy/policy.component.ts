8606f0ac2346a0a49e42d6fd54680c6f8dbc71ec
ACKRONNE <75231354+ACKRONNE@users.noreply.github.com>
2025-01-02 15:27:44 -0400

    ✨ FEAT:F-GRUAS-CreatePolicy

--- src/app/pages/functionalities/policy/policy.component.ts
+++ src/app/pages/functionalities/policy/policy.component.ts
@@ -1,28 +1,56 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
-import { GoogleMapsModule } from '@angular/google-maps';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
-import { TablerIconsModule } from 'angular-tabler-icons';
+import { MatTableModule } from '@angular/material/table';
+import { MaterialModule } from 'src/app/material.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+
+export interface policyData {
+  imagePath: string;
+  name: string;
+  kmcoverage: string;
+  moneycoverage: string;
+}
+
+interface Type {
+  value: string;
+}
+
+const POLICY_DATA: policyData[] = [
+  { imagePath: 'assets/images/policy/bronce.png', name: 'Bronce', kmcoverage: '100,000 km', moneycoverage: '$100,000'},
+  { imagePath: 'assets/images/policy/silver.png', name: 'Plata', kmcoverage: '300,000 km', moneycoverage: '$300,000'},
+  { imagePath: 'assets/images/policy/oro.png', name: 'Gold', kmcoverage: '500,000 km', moneycoverage: '$500,000'},];
+
 
 @Component({
   selector: 'app-policy',
   standalone: true,
   imports: [
-    GoogleMapsModule,
-    FormsModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatFormFieldModule,
+    MatTableModule,
+    CommonModule,
     MatCardModule,
-    MatInputModule,
-    TablerIconsModule,
+    MaterialModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
   ],
   templateUrl: './policy.component.html',
 })
 export class AppPolicyComponent {
-  
+    // Table
+    displayedColumns1: string[] = ['class', 'name', 'kmcoverage', 'moneycoverage', 'budget'];
+    dataSource1 = POLICY_DATA;
+    // End Table
+
+    // Select
+    type: Type[] = [
+      { value: 'Oro' },
+      { value: 'Plata' },
+      { value: 'Bronce' },
+    ];
+    selectedType = this.type[0].value;
+    // End Select
+
 }
