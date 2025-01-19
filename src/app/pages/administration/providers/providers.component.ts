995d9df49e6caba7a6c3a181e444e6c00eeb56eb
ACKRONNE <ACKRONNE@GMAIL.COM>
2025-01-19 12:20:40 -0400

    ⬆️ INTEGRATION:F-GRUAS-PutProviderConnection

--- src/app/pages/administration/providers/providers.component.ts
+++ src/app/pages/administration/providers/providers.component.ts
@@ -6,9 +6,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { providerData } from 'src/app/domain/provider.domain';
+import { userData } from 'src/app/domain/user.domain';
+
 import { ApiProviderService } from 'src/app/services/provider.service';
+import { ApiUserService } from 'src/app/services/user.service';
 
 interface isActive {
   value: boolean;
@@ -26,6 +32,7 @@ interface isActive {
     MatIconModule,
     MatMenuModule,
     MatButtonModule,
+    ReactiveFormsModule
   ],
   templateUrl: './providers.component.html',
 })
@@ -34,18 +41,77 @@ export class AppProvidersComponent implements OnInit {
     displayedColumns: string[] = ['imagePath', 'rif', 'name', 'budget'];
     dataSource: providerData[] = [];
 
+    user: userData[] = [];
+    userOptions: { value: string; viewValue: string, idValue: string }[] = [];
+    selectedUser: string | null = null;
+
+    form: FormGroup;
+    
     providerStatus: string[] = ['Activo', 'Inactivo'];
 
-    constructor (private apiProviderService: ApiProviderService) {}
+    constructor (
+      private apiProviderService: ApiProviderService,
+      private ApiUserService: ApiUserService,
+      private fb: FormBuilder,
+      private snackBar: MatSnackBar      
+    ) {
+        this.form = this.fb.group({
+          provider: [null, Validators.required],
+          rif: ['', Validators.required],
+          image: ['../../../../assets/images/profile/user-3.jpg'],
+        });
+    }
+
+    get f() {
+      return this.form.controls;
+    }
 
     ngOnInit(): void {
         this.loadProviders();
+
+        this.apiProviderService.getUser().subscribe((users) => {
+          this.user = users.filter(user => user.role === 'provider');
+          this.userOptions = this.user.map((user) => ({
+            value: user.name as string,
+            viewValue: user.name,
+            idValue: user.id as string
+          }));
+        });
     }
 
     loadProviders(): void {
         this.apiProviderService.getProviders().subscribe((providers) => {
             this.dataSource = providers;
         });
+    }
+
+    createProvider(): void {
+      const { id, name } = this.form.value.provider;
+      const rif = this.form.value.rif;
+      const image = this.form.value.image;
+  
+      const newProvider: providerData = {
+        id: id,
+        name: name,
+        rif: rif,
+        image: image,
+      }
+      this.apiProviderService.createProvider(newProvider).subscribe(
+        (data) => {
+          console.log('Proveedor creado:', data);
+          this.dataSource.push(data);
+          this.snackBar.open('Proveedor creado', 'Cerrar', {
+            duration: 2000,
+          });
+          this.loadProviders();
+        },
+        (error) => {
+          console.error('Error al crear proveedor:', error);
+          this.snackBar.open('Error al crear proveedor', 'Cerrar', {
+            duration: 2000,
+          });
+        }
+      );
     }
 
     onFileSelected(event: Event): void {
--- src/app/services/provider.service.ts
+++ src/app/services/provider.service.ts
@@ -6,6 +6,7 @@ import { catchError, map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 import { providerData } from '../domain/provider.domain';
+import { userData } from '../domain/user.domain';
 
 @Injectable({
     providedIn: 'root',
@@ -21,4 +22,40 @@ import { providerData } from '../domain/provider.domain';
         headers,
       });
     }
-  }
+
+    createProvider(provider: providerData): Observable<providerData> {
+      const token = this.authService.getToken();
+      const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  
+      console.log('Datos enviados:', provider);
+  
+      return this.http
+        .post<providerData>(`${this.apiUrl}/providers-ms/provider`, provider, { headers })
+        .pipe(
+          catchError((error: any) => {
+            console.error('Error al agregar proveedor:', error);
+            if (error.error && error.error.errors) {
+              console.error('Validation errors:', error.error.errors);
+            }
+            return throwError(error);
+          })
+        );
+    }
+
+    getUser(): Observable<userData[]> {
+        const token = this.authService.getToken();
+        if (!token) {
+        return throwError('Token not found');
+        }
+        const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+        return this.http
+        .get<userData[]>(`${this.apiUrl}/users-ms/user?limit=100`, { headers })
+        .pipe(
+            catchError((error) => {
+            console.error('Error obteniendo los usuarios:', error);
+            return throwError(error);
+            })
+        );
+    }
+}
